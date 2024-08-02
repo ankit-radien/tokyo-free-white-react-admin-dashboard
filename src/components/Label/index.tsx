3b9bc8748ad6483d34b8460995164ef3ee4cb0a6
ankit-radien <148485793+ankit-radien@users.noreply.github.com>
2024-08-02 18:24:58 +0530

    Update index.tsx

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
-//test2zxc
+//test2zxcsdf
 interface LabelProps {
   className?: string;
   color?:
